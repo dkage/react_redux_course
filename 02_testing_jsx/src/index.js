5e28fa27ea8d096cf7694e5623e2172ed89698bd
Danilo Cunha <danilokage@gmail.com>
2019-10-16 20:55:06 -0300

    Add variable to button text

--- 02_testing_jsx/src/index.js
+++ 02_testing_jsx/src/index.js
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom';
 
 // React component
 const App = () => {
+    const buttonText = 'Click right here';
+
     return (
       <div>
           <label className={"label"} for="name"> {/*Use 'className' instead 'class' to avoid collision with class JS*/}
               Enter name:
           </label>
           <input id={"name"} type={"text"}/>
-          <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+          <button style={{backgroundColor: 'blue', color: 'white'}}>
+              {buttonText}
+          </button>
       </div>
     );
 };
