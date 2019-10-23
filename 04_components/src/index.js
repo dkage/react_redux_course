83a53b8ce787ca98debc16904561f12d75fe80cb
Danilo Cunha <danilokage@gmail.com>
2019-10-23 20:38:19 -0300

    Set component inside component as children a props

--- 04_components/src/index.js
+++ 04_components/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker'
 
 // Creates a functional component
 const App = () => {
     return (
         <div className={'ui container comments'}>
-            <CommentDetail userAvatar={faker.image.avatar()} author="Sam"  date="Yesterday"       text={faker.lorem.paragraph()}/>
-            <CommentDetail userAvatar={faker.image.avatar()} author="Jane" date="Eight hours ago" text={faker.lorem.paragraph()}/>
-            <CommentDetail userAvatar={faker.image.avatar()} author="Bam"  date="Five hours ago"  text={faker.lorem.paragraph()}/>
-            <CommentDetail userAvatar={faker.image.avatar()} author="Tim"  date="One second ago"  text={faker.lorem.paragraph()}/>
+            <ApprovalCard>
+                <CommentDetail userAvatar={faker.image.avatar()} author="Sam"  date="Yesterday"       text={faker.lorem.paragraph()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail userAvatar={faker.image.avatar()} author="Jane" date="Eight hours ago" text={faker.lorem.paragraph()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail userAvatar={faker.image.avatar()} author="Bam"  date="Five hours ago"  text={faker.lorem.paragraph()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail userAvatar={faker.image.avatar()} author="Tim"  date="One second ago"  text={faker.lorem.paragraph()}/>
+            </ApprovalCard>
         </div>
 
     );
