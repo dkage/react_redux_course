2d37cc675c9c251499c833b9787e2ccd734fc9ca
Danilo Cunha <danilokage@gmail.com>
2020-02-07 18:57:12 -0300

    Add error msg printing for browser geolocation

--- 05_seasons/src/index.js
+++ 05_seasons/src/index.js
@@ -6,23 +6,22 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null,
+                       errorMsg: ''
+        };
 
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude})
-            },
-            (err)     => console.log(err)
+            (position) => { this.setState({lat: position.coords.latitude} )}
+            , (err) => { this.setState( {errorMsg: err.message} )}
         );
-
-
     }
 
 
-
     render () {
-
-        return <div>Latitude: {this.state.lat}</div>;
+        return (<div>Latitude: {this.state.lat}
+                    <br/>
+                     Error: {this.state.errorMsg}
+                </div>);
     }
 }
 
